d0c0179dabe2db6c5ee5a359221508981e63f39c
Nese84 <nbserbest84@gmail.com>
2023-02-18 18:03:11 +0300

    tour-place

---
+++ src/compenents/header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Card from '../card/Card'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+        <h1>Popular Tour Places</h1>
+        <Card/>
+       
+    </div>
+  )
+}
+
+export default Header
